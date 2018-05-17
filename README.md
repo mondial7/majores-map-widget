0414cc9f73fa64b8a4b554f0c6e0a8042d2fad6d
Marco <mondial95@gmail.com>
2018-05-17 19:57:02 +0200

    🎉 🌊 🐙

---
+++ README.md
@@ -0,0 +1,32 @@
+# My Bae
+Something about my dev stack
+
+## Install
+
+clone this 👾
+```
+git clone https://github.com/mondial7/my-bae.git
+cd my-bae
+npm install
+```
+
+## Developing
+
+```
+npm run forest
+```
+(... gonna watch you 🧐)
+
+## Testing 
+
+```
+npm test
+```
+(surprise surprise 🤓)
+
+## Wrapping up
+
+```
+npm run build
+```
+(or just do nothing if Forest was running - after all... Run, Forest, run!)
---
+++ src/data.js
@@ -0,0 +1,24 @@
+const MAJORE_MAP_DATA = [
+  {
+    university: 'Free University of Bolzano Bozen',
+    logo: 'https://upload.wikimedia.org/wikipedia/commons/6/6d/UniBZ-Logo.svg',
+    description: 'aodoaskd aosd kmasdkmaskd msakmdoadamod asodkoasmdkomasokd',
+    students: [
+      {
+        name: 'Matteo Redaelli',
+        diploma: '',
+        degree: '',
+        enrollment: '',
+        graduation: null,
+        socials: [
+          {
+            name: 'facebook',
+            link: 'https://www.facebook.com/matteo.redaelli.54',
+          },
+        ]
+      },
+      // another student ...
+    ],
+  },
+  // another uni ...
+]
